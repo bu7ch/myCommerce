fc859b4aae6a248945559fb68d1a20cb107ef475
Rodolphe <dutchr@live.fr>
2020-06-10 16:34:32 +0200

    protect some routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { LoginComponent } from './components/login/login.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { MyOrdersComponent } from './components/my-orders/my-orders.component';
 import { CheckOutComponent } from './components/check-out/check-out.component';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
 
 
 const routes: Routes = [
@@ -14,14 +16,15 @@ const routes: Routes = [
   {path:"login", component: LoginComponent},
   {path:"shopping-cart", component: ShoppingCartComponent},
 
-  {path:"my-orders", component: MyOrdersComponent},
-  {path:"check-out", component: CheckOutComponent},
+  {path:"my-orders", component: MyOrdersComponent, canActivate: [AuthGuard]},
+  {path:"check-out", component: CheckOutComponent, canActivate: [AuthGuard]},
   
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthService, AuthGuard]
 })
 export class AppRoutingModule { }
