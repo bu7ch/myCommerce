78b9b42fa000c69560c7a23d50fc1e606b0dad0a
Rodolphe <dutchr@live.fr>
2020-06-10 15:15:35 +0200

    display my account & logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+user: firebase.User ;
+  constructor(private auth: AngularFireAuth) {
+    auth.authState.subscribe(user=> this.user = user);
+   }
 
   ngOnInit(): void {
   }
-
+  logout() {
+    this.auth.signOut();
+  }
 }
